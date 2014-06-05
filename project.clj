e565219969ec79002629ba942537a2e993d8b588
Kyle Oba <koba@mudphone.com>
2014-06-04 22:26:38 -1000

    Add basic Mies-Om + Kickoff project.

---
+++ project.clj
@@ -0,0 +1,23 @@
+(defproject agora "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2227"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+                 [om "0.6.4"]
+                 [ring "1.3.0"]]
+  :plugins [[lein-cljsbuild "1.0.3"]
+            [lein-ring "0.8.10"]]
+  :hooks [leiningen.cljsbuild]
+  :source-paths ["src/clj"]
+  :cljsbuild { 
+    :builds {
+      :main {
+        :source-paths ["src/cljs"]
+        :compiler {:output-to "resources/public/js/cljs.js"
+                   :optimizations :simple
+                   :pretty-print true}
+        :jar true}}}
+  :main agora.server
+  :ring {:handler agora.server/app})
+
