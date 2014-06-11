78852866b8840b479c0c50a2c9f8c97e4c5e38c8
Kyle Oba <koba@mudphone.com>
2014-06-10 23:00:28 -1000

    Fix stop-server, so it works with http-kit.

--- src/clj/agora/repl.clj
+++ src/clj/agora/repl.clj
@@ -13,5 +13,8 @@
     (println (str "You can view the site at http://localhost:" port))))
 
 (defn stop-server []
-  (.stop @server)
-  (reset! server nil))
+  (when-not (nil? @server)
+    ;; graceful shutdown: wait 100ms for existing requests to be finished
+    ;; :timeout is optional, when no timeout, stop immediately
+    (@server :timeout 100)
+    (reset! server nil)))
