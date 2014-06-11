a6d161d8be0df70d340823a69c75ca8e4ec54869
Kyle Oba <koba@mudphone.com>
2014-06-10 23:01:25 -1000

    Add initialization of database, in case we don't have one yet.
    This is probably going to change, since it's all hard-coded.

--- src/clj/agora/db/grid.clj
+++ src/clj/agora/db/grid.clj
@@ -4,7 +4,23 @@
    [agora.db.result :as ar]
    [datomic.api :as d]))
 
-(def conn nil)
+(def db-uri "datomic:free://localhost:4334/agora")
+
+(defn initialize-db
+  "Create a blank Datomic DB"
+  [uri]
+  (d/create-database uri)
+  (let [conn (d/connect uri)
+        schema (load-file "resources/datomic/agora_schema.edn")]
+    (d/transact conn schema)
+    conn))
+
+(def conn
+  (try
+    (d/connect db-uri)
+    (catch Exception e
+      (println "Initializing Datomic!")
+      (initialize-db db-uri))))
 
 (def DEFAULT-GRID-NAME "agora")
 
